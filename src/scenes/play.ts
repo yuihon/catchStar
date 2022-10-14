06a546b5ddd7387da28dc760937cc826dc39f920
yuihon <1797431870@qq.com>
2022-10-14 10:14:34 +0800

    实现随时间随机个数刷出星星功能

--- src/scenes/play.ts
+++ src/scenes/play.ts
@@ -10,12 +10,13 @@ export default class Play extends Phaser.Scene {
  public l;
  public isClimb;
  public topgrounds;
- public stars;
  public randomStarNum;
+ public timeClock;
+ public curStarNum;
+ public freshTime;
   constructor() {
     super("Play");
     this.stores=[];
-    this.stars=[];
   }
   preload() {
 
@@ -23,26 +24,30 @@ export default class Play extends Phaser.Scene {
   create() {
       this.player=new Player(this,config.width/2,config.height-314,"player");
       this.player.setClimbForce(1.8);
-
+      this.curStarNum=0;
+      this.freshTime=0;
+      this.randomStarNum=1;
       this.createStores();
       this.createGrounds();
-      this.randomStar();
-      this.createStars();
-      
+    
 
       this.input.on("gameobjectup",this.clickStore,this);
       this.l=this.add.line(75,0,0,0,140,0,0xeb981b);
       this.l.setLineWidth(10,10);
       this.l.setVisible(false);
       this.isClimb=false;
+
+      this.timeClock=0;
       proxy.on(CLICK_STORE,this.climb,this);
     }
 
-  update() {
+  update(time: number, delta: number) {
+    this.timeClock=Math.round(time/1000);
+    this.timeControlStarNum();
+    this.freshStar(delta);
     if(this.isClimb==true)
     {
       this.l.setTo(this.player.x,this.player.y-58,this.targetStore.x,this.targetStore.y+26);
-      
     }
   }
   clickStore(pointer,store){
@@ -61,7 +66,6 @@ export default class Play extends Phaser.Scene {
     this.stores[2]=new Store(this,450,500,"store");
     this.stores[3]=new Store(this,650,200,"store");
     this.stores[4]=new Store(this,850,500,"store");
-    // this.stores[5]=new Store(this,750,200,"store");
     for(let i=0;i<5;i++)
     {
       this.stores[i].setInteractive();
@@ -80,15 +84,38 @@ export default class Play extends Phaser.Scene {
     this.physics.add.collider(this.player,this.topgrounds,this.touchGround,null,this);
 
   }
-  createStars()
+  createStar()
   {
-    for(let i=0;i<this.randomStarNum;i++)
+    if(this.curStarNum<this.randomStarNum)
     {
-      this.stars[i]=new Star(this,250,400,"star");
+      var stars=new Star(this,250,400,"star");
+      this.curStarNum++;
     }
   }
-  randomStar(){
-     this.randomStarNum=5;
+  freshStar(delta){
+    this.freshTime=this.freshTime+delta/1000;
+    if(Math.floor(this.freshTime)>=2)
+    {
+      this.createStar();
+      this.freshTime=0;
+    }
+  }
+  timeControlStarNum(){
+    if(this.timeClock>=5){
+      this.randomStarNum=2;
+    }
+    if(this.timeClock>=10){
+      this.randomStarNum=3;
+    }
+    if(this.timeClock>=15){
+      this.randomStarNum=4;
+    }
+    if(this.timeClock>=20){
+      this.randomStarNum=5;
+    }
+    if(this.timeClock>=25){
+      this.randomStarNum=6;
+    }
   }
 }
 
